9150b081e0145707c98c818f273e0d576997650e
Reenberg <khreenberg@gmail.com>
2012-10-27 10:56:46 +0200

    Initial commit.

---
+++ TestProject1/src/cs/easv/b2/MainClass.java
@@ -0,0 +1,13 @@
+package cs.easv.b2;
+
+public class MainClass {
+
+	public static void main( String[] args ){
+
+		for( int i = 1; i <= 10; ++i ){
+			System.out.printf( "%3s. Just testing!\n", i );
+		}
+
+	}
+
+}
