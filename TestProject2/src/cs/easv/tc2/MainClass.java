bb07bb47823c4c0a05ea4f4e64375a1a971bde7d
Reenberg <khreenberg@gmail.com>
2012-10-27 12:10:17 +0200

    Add TestProject2

---
+++ TestProject2/src/cs/easv/tc2/MainClass.java
@@ -0,0 +1,24 @@
+package cs.easv.tc2;
+
+/**
+ * This is the Main class of 'TestProject1', which serves the purpose of testing
+ * the possibilities of having several projects in one repo on GitHub.com
+ * 
+ * @author KHR
+ * 
+ */
+public class MainClass {
+
+	public static void main( String[] args ){
+		String l1 = "____________________________________________";
+		String l2 = "/~~~~~~~~\\__/~~~~~~\\___/~~~~~~\\__/~~\\__/~~\\_";
+		String l3 = "/~~\\_______/~~\\__/~~\\_/~~\\_______/~~\\__/~~\\_";
+		String l4 = "/~~~~~~\\___/~~~~~~~~\\__/~~~~~~\\__/~~\\__/~~\\_";
+		String l5 = "/~~\\_______/~~\\__/~~\\_______/~~\\__/~~\\/~~\\__";
+		String l6 = "/~~~~~~~~\\_/~~\\__/~~\\__/~~~~~~\\____/~~~~\\___";
+		String l7 = "____________________________________________";
+		
+		System.out.printf("%s\n%s\n%s\n%s\n%s\n%s\n%s", l1, l2, l3, l4, l5, l6, l7);
+	}
+
+}
