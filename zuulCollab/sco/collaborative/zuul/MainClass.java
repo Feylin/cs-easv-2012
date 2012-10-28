363a387d3532ac5e6093aa2c233b4d33a42b902e
Reenberg <khreenberg@gmail.com>
2012-10-28 08:45:38 +0100

    Add comment to experiment with branching

--- zuulCollab/sco/collaborative/zuul/MainClass.java
+++ zuulCollab/sco/collaborative/zuul/MainClass.java
@@ -4,6 +4,11 @@ public class MainClass {
 
 	public static void main( String[] args ){
 
+		/*
+		 * This is just some text so that the MainClass.java changes, 
+		 * and I can push it to a new branch and eventually merge it 
+		 * into origin/master instead of pushing it directly there.
+		 */
 		new Game().play();
 
 	}
